0f82f47428e5eca396a5ce4ecaf235c016a96c7c
Atik hasan <atikh9979@gmail.com>
2025-05-20 18:21:12 +0600

    Initial Commit

---
+++ Dont Animation Loading Animation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dont Animation Loading Animation
+//
+//  Created by Atik Hasan on 5/20/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
